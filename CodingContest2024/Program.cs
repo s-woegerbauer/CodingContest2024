ff72b911e9bb4687aa5aa9e4f28d51ae9fe745e9
s-woegerbauer <120839159+s-woegerbauer@users.noreply.github.com>
2024-04-10 10:42:26 +0200

    Explaination added

--- CodingContest2024/Program.cs
+++ CodingContest2024/Program.cs
@@ -2,6 +2,33 @@
 
 public static class Program
 {
+    /// <summary>
+    /// The Input in the Coding Contest is given always the same:
+    /// 3 [Number of how many lines the input is]
+    /// Line 1
+    /// Line 2
+    /// Line 3
+    /// 2 [Number of how many lines the next input is]
+    /// Line 1 of the next input
+    /// Line 2 of the next input
+    /// ... [this can go on for many inputs]
+    ///
+    /// To make this easy the InputOutputHelper needs a parameter List of functions, these functions are in the order of the input to process e.g.:
+    /// ParseOneFunc(); --> Will parse the first part of the input (in the above example 3 Lines)
+    /// ParseTwoFunc(); --> Will parse the second part of the input (in the above example 2 Lines)
+    ///
+    /// To process the parsed input a Solution function is needed --> List[string] Solution(Parser parser)
+    ///
+    /// InputOutputHelper.Process(Solution, new Parser(ParseOneFunc(), ParseTwoFunc(), ...));
+    ///
+    /// The function "parser.Go()" will return a list of objects, the parsed objects from the functions given to the parser
+    ///
+    /// The InputOutputHelper.Process( ... ) will automatically process all input files in the ./Input/ folder and will print the List[string] given from the solution
+    /// foreach Input into the ./Output/ folder with the same name as the input file
+    ///
+    /// The Grid class will help with 2d array stuff like path finding, neighbours, boundary management, etc.
+    /// </summary>
+    
     public static void Main()
     {
         List<Func<List<string>, object>> functions = new List<Func<List<string>, object>>();
---
+++ CodingContest2024/README.md
@@ -0,0 +1,20 @@
+﻿# Coding Contest 2024 (April)
+
+## Introduction
+
+The Coding Contest is an event that takes place twice a year. It is hosted by the company Cloudflight. The idea is simple, coders get an input and have to process it like in the level description to get an output and proceed to the next level.
+
+## Why use a Preset?
+
+Using a preset gives you certain advantages. The parsing and processing of inputs gets way easier and you only have to worry about solving the problem.
+
+## How to use this Preset?
+
+1. Download this Preset
+2. Open it in an IDE of your choice
+3. Copy the input files into the ./Input/ folder
+4. Write parser functions to process the input
+5. Write a solution function to solve the problem
+6. Run the program
+7. Upload the output files in the ./Output/ folder
+8. Same again in the next levels
